094f44ed1c8060a61aa82308b88bcd63745ab7c6
Mastersam <abadasamuelosp@gmail.com>
2020-09-12 03:47:23 -0700

    server

---
+++ config.py
@@ -0,0 +1,21 @@
+# bot interval and timeout
+bot_interval = 3
+bot_timeout = 30
+current_result = ""
+
+# set environment variables
+# bot api
+api_key = "1314384954:AAHcFphPjJaMK5oxJYEDDjEauhygCZzyMD4"
+
+# database details
+db_user = "postgres"
+db_password = "mastersam"
+db_host = "localhost"
+db_port = ""
+db_name = "futbalbot"
+
+# us sports news
+ussports = "http://newsapi.org/v2/top-headlines?country=us&q=sports&apiKey=14404d1267a94b8fa3d50e5364ed10d4"
+
+# uk sports news
+uksports = "http://newsapi.org/v2/top-headlines?country=uk&q=sports&apiKey=14404d1267a94b8fa3d50e5364ed10d4"
